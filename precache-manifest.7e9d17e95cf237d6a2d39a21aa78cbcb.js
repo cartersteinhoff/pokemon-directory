430763ca84ef89f9c53cdc8436857fb0d11a98fc
Carter Steinhoff <carter@binary.local>
2020-07-22 15:44:34 -0700

    Updates

---
+++ precache-manifest.7e9d17e95cf237d6a2d39a21aa78cbcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7613f0f0508f0c9fb08517240d09794",
+    "url": "/pokemon-directory/index.html"
+  },
+  {
+    "revision": "7a518febe4421be5b243",
+    "url": "/pokemon-directory/static/css/main.e3691ddc.chunk.css"
+  },
+  {
+    "revision": "50ecf1c70ec6894fd047",
+    "url": "/pokemon-directory/static/js/2.89fc9e7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon-directory/static/js/2.89fc9e7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a518febe4421be5b243",
+    "url": "/pokemon-directory/static/js/main.9583a398.chunk.js"
+  },
+  {
+    "revision": "55ae1807c71ff747d009",
+    "url": "/pokemon-directory/static/js/runtime-main.acd6e4d5.js"
+  }
+]);
